961f60ecf5015eb001d97d2cf7a54457ea59d07c
Jay Makwana <jaymakwana984@gmail.com>
2023-03-06 15:36:16 +0530

    Done the setup and dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import "./App.css";
+import "./Style/App.css";
+import Navbar from "./Component/Navbar/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="herder-part">
+        <Navbar />
+      </div>
+      <div className="main-content">main Content</div>
+      <div className="footer-part">Footer</div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Utilities/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBGGycRDcx7VfKykBSekJDNJhNKmqpsBJM",
+  authDomain: "react-chat-app-1a6aa.firebaseapp.com",
+  projectId: "react-chat-app-1a6aa",
+  storageBucket: "react-chat-app-1a6aa.appspot.com",
+  messagingSenderId: "933385356933",
+  appId: "1:933385356933:web:23f4737e049a851b358bec",
+};
+
+const app = initializeApp(firebaseConfig);
