fb89760d6805282f072d01e80aa1424614974487
Jay Makwana <jaymakwana984@gmail.com>
2023-03-09 15:25:07 +0530

    started the chat app design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./style/App.css";
 import Header from "./Component/Navbar/Header";
+import Chat from "./pages/Chat";
 
 function App() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
+      <Chat />
     </>
   );
 }
---
+++ src/pages/Chat.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Chat() {
+  return <div>this is the chat </div>;
+}
