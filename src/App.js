34d98adf087fa04d33617f04aecd25756e487ba7
Jay Makwana <jaymakwana984@gmail.com>
2023-03-09 15:21:48 +0530

    Remove the unuse pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from "react";
 import "./style/App.css";
 import Header from "./Component/Navbar/Header";
-// import Footer from "./Component/Footer/Footer";
-import styled from "styled-components";
-
-//
 
 function App() {
   return (
--- src/Pages/ChatBoard.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ChatBoard() {
-  return <div></div>;
-}
