f86d97e419ea1f06f43b5e8a045cfab5ab1069a1
Jay Makwana <jaymakwana984@gmail.com>
2023-03-10 11:10:17 +0530

    start chatbar

--- src/Component/Chatbar.js
+++ src/Component/Chatbar.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Chatbar() {
-  return <div></div>;
+  return <div>this is chatbar</div>;
 }
--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Sidbar from "../Component/Sidbar";
+import Chatbar from "../Component/Chatbar";
 
 export default function Chat() {
   return (
     <div>
-      <Sidbar />
+      {/* <Sidbar /> */}
+      <Chatbar />
     </div>
   );
 }
