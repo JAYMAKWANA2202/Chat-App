a91898068ebbb5e504dfce02dd24cacc39df279e
Jay Makwana <jaymakwana984@gmail.com>
2023-03-21 17:22:32 +0530

    connect  a login page with firebase

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,8 +5,37 @@ import styled from "styled-components";
 import { Whatsapp } from "react-bootstrap-icons";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/esm/Button";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
+import { app } from "../utilities/firebase";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const navigate = useNavigate();
+  const auth = getAuth(app);
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleChange(e) {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, values.email, values.password)
+      .then(async () => {
+        console.log("welcome");
+        if (signInWithEmailAndPassword) {
+          navigate("/chat");
+        }
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
   return (
     <>
       <Headerpart>
@@ -27,7 +56,7 @@ export default function Login() {
         <Container>
           <Center>
             <FormContainer>
-              <Form>
+              <Form onSubmit={handleSubmit}>
                 <h1>Login Page</h1>
 
                 <Form.Group controlId="formBasicEmail">
@@ -35,8 +64,10 @@ export default function Login() {
                   <Form.Control
                     type="email"
                     name="email"
+                    onChange={handleChange}
                     placeholder="Enter email"
                     autoComplete="off"
+                    value={values.email}
                   />
                 </Form.Group>
 
@@ -45,13 +76,20 @@ export default function Login() {
                   <Form.Control
                     type="password"
                     name="password"
+                    onChange={handleChange}
                     placeholder="Password"
                     autoComplete="off"
+                    value={values.password}
                   />
                 </Form.Group>
 
-                <Button variant="success" type="submit" className="btn mt-3">
-                  Sign Up
+                <Button
+                  variant="success"
+                  type="submit"
+                  className="btn mt-3"
+                  onClick={handleSubmit}
+                >
+                  Login
                 </Button>
                 <Form.Group controlId="formBasicPassword" className="mt-2">
                   <p>Do you have account? SignUp</p>
